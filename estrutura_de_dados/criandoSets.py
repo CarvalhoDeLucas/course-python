3ca811a946f521a14bf0b4acaa04f0fe177a5155
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2023-04-13 23:43:05 -0300

    Array e set

---
+++ estrutura_de_dados/criandoSets.py
@@ -0,0 +1,10 @@
+lista1 = [10, 20, 30, 40, 50]
+lista2 = [10, 20, 60, 70]
+
+num1 = set(lista1)
+num2 = set(lista2)
+
+print(num1 | num2)  # Union
+print(num1 - num2)  # Difference
+print(num1 ^ num2)  # Symmetric Difference
+print(num1 & num2)  # And
