25e0ff0473884a64116b7a60b169fbebf0ab50c7
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2023-05-03 23:14:38 -0300

    Classes

---
+++ OOP/PrimeiraFuncao.py
@@ -0,0 +1,10 @@
+class Funcionarios:
+    nome = 'Lucas'
+    sobrenome = 'Carvalho'
+    data_nascimento = '14/01/2001'
+
+usuario1 = Funcionarios()
+
+print(usuario1.nome)
+print(usuario1.sobrenome)
+print(usuario1.data_nascimento)
