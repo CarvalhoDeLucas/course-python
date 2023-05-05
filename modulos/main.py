bafb08570a7d4a10cbb132cadc518645489e61c9
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2023-05-05 19:04:22 -0300

    Modulos

--- modulos/main.py
+++
@@ -1,9 +0,0 @@
-# Importa todos as funcoes da classe
-#import funcoes
-
-#funcoes.somar()
-
-# Importa funcoes especificas da classe funcoes
-from funcoes import somar
-
-somar()
---
+++ modulos/modulos/main.py
@@ -0,0 +1,7 @@
+from funcoes import somar, multiplicar
+# Precisa do __init__.py para reconhecer o modulo cliente
+from itens.cadastro import cliente
+
+somar()
+multiplicar()
+cliente()
